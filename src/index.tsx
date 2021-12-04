5e709e3b2c436c46aa27f2fde1e98910df804ff7
Francis Doege <francis.doege@web.de>
2021-12-04 22:49:18 +0100

    Add GH-Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+     <ToastContainer />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
