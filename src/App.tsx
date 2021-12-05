80d63ff250cb39407e34af0c88cc352c691dfe38
Francis Doege <francis.doege@web.de>
2021-12-05 01:04:32 +0100

    Added CNAME

--- src/App.tsx
+++ src/App.tsx
@@ -52,19 +52,28 @@ function App() {
 
           <div className="row vertical-center justify-content-center">
             <div className="p-5 mb-4 bg-light rounded-3 bg-opacity-75 col-md-6 ">
-    
-                <h1 className="display-5 fw-bold">Wichtelmaschine</h1>
-                <p className="fs-4">Für wen bin ich in diesem Jahr der Wichtel?</p>
 
-                {(!wichtel && !isLoading) &&
-                  <input placeholder="Hier den Code eingeben" type="text" className="form-control form-control-lg" onChange={(e) => setInputValue(e.target.value)} />
-                }
-                {(!wichtel && isLoading) &&
-                  "Lädt...."
-                }
-                {wichtel &&
-                  `${wichtel}`
-                }
+              <h1 className="display-5 fw-bold">Wichtelmaschine</h1>
+              <p className="fs-4">Für wen bin ich in diesem Jahr der Wichtel?</p>
+
+              {(!wichtel && !isLoading) &&
+                <input placeholder="Hier den Code eingeben" type="text" className="form-control form-control-lg" onChange={(e) => setInputValue(e.target.value)} />
+              }
+              {(!wichtel && isLoading) &&
+                <div className="d-flex justify-content-center">
+                  <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                </div>
+              }
+              {wichtel &&
+                <>
+                <p className="fs-5">Du beschenkst in diesem Jahr:</p>
+                <div className="text-center">
+                  <h2>{wichtel}</h2>
+                </div>
+                </>
+              }
 
             </div>
 
